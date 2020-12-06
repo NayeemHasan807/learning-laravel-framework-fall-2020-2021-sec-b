8a1b6407e0b709e62471c459d161f1e672f572bd
NayeemHasan807 <moonnayeem87@gmail.com>
2020-12-06 22:12:23 +0600

    {1:array_crud view details}

---
+++ array_crud/resources/views/home/create.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Create</title>
+</head>
+<body>
+	<form method="post">
+		<fieldset>
+			<legend>Create</legend>
+		<table>
+			<tr>
+				<td>ID</td>
+				<td><input type="text" name="id"></td>
+			</tr>
+			<tr>
+				<td>Name</td>
+				<td><input type="password" name="id"></td>
+			</tr>
+			<tr>
+				<td>CGPS</td>
+				<td><input type="text" name="cgps"></td>
+			</tr>
+			<tr>
+				<td>Email</td>
+				<td><input type="text" name="email"></td>
+			</tr>
+			<tr>
+				<td></td>
+				<td><input type="submit" name="submit" value="Submit"></td>
+			</tr>
+		</table>
+		</fieldset>
+	</form>
+</body>
+</html>
---
+++ array_crud/resources/views/home/details.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Student List</title>
+</head>
+<body>
+	<a href="/stdlist">Back</a> |
+	<a href="/logout">logout</a>
+	<br>
+
+	<table border="1">
+		<tr>
+			<td>Id</td>
+			<td>:</td>
+			<td>{{$students[0]['id']}}</td>
+		</tr>
+		<tr>
+			<td>Name</td>
+			<td>:</td>
+			<td>{{$students[0]['name']}}</td>
+		</tr>
+		<tr>
+			<td>CGPS</td>
+			<td>:</td>
+			<td>{{$students[0]['cgpa']}}</td>
+		</tr>
+		<tr>
+			<td>Email</td>
+			<td>:</td>
+			<td>{{$students[0]['email']}}</td>
+		</tr>
+		<tr>
+
+		</tr>
+	</table>
+</body>
+</html>
